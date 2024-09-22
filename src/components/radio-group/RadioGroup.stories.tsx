82c98275a282870e73a495fefd26a0288d561e57
Julia <veluat@gmail.com>
2024-09-23 00:50:25 +0300

    INC-7 fix: implement correct RadioGroup structure

--- src/components/radio-group/RadioGroup.stories.tsx
+++ src/components/radio-group/RadioGroup.stories.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react'
-
-import { Meta, StoryObj } from '@storybook/react'
-
-import { RadioGroup, RadioGroupItem } from './RadioGroup'
+import {Meta, StoryObj} from '@storybook/react'
+import {RadioGroup} from './RadioGroup'
+import {useState} from 'react'
 
 const meta = {
   component: RadioGroup,
@@ -14,45 +12,35 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 const options = [
-  { label: 'Option 1', value: 'option-1' },
-  { label: 'Option 2', value: 'option-2' },
-  { label: 'Option 3', value: 'option-3' },
+  {label: 'Option 1', value: 'option-1'},
+  {label: 'Option 2', value: 'option-2'},
+  {label: 'Option 3', value: 'option-3'},
 ]
 
 export const Default: Story = {
+  args: {
+    options: options,
+  },
   render: () => {
     const [value, setValue] = useState<string>('option-2')
 
     return (
-      <RadioGroup defaultValue={'option-2'} onValueChange={setValue}>
-        {options.map(option => (
-          <div className={'flex items-center gap-2'} key={option.value}>
-            <RadioGroupItem id={option.value} value={option.value} />
-            <label className={'text-regular_14 text-light-100'} htmlFor={option.value}>
-              {option.label}
-            </label>
-          </div>
-        ))}
+      <div className={'flex flex-col w-full gap-10'}>
+        <RadioGroup
+          options={options}
+          defaultValue={value}
+          onValueChange={setValue}
+        />
         <span className={'text-h2 text-light-100 mt-5'}>Selected Item: {value}</span>
-      </RadioGroup>
+      </div>
     )
   },
 }
 
 export const Disabled: Story = {
-  render: () => {
-    return (
-      <RadioGroup defaultValue={'option-2'}>
-        {options.map(option => (
-          <div className={'flex items-center gap-2'} key={option.value}>
-            <RadioGroupItem disabled id={option.value} value={option.value} />
-            <label className={'text-regular_14 text-light-900'} htmlFor={option.value}>
-              {option.label}
-            </label>
-          </div>
-        ))}
-        <span className={'text-light-100 text-h2 mt-5'}>All options are disabled</span>
-      </RadioGroup>
-    )
+  args: {
+    disabled: true,
+    options: options,
+    defaultValue: options[1]?.value,
   },
 }
--- src/components/radio-group/RadioGroup.tsx
+++ src/components/radio-group/RadioGroup.tsx
@@ -1,16 +1,47 @@
 'use client'
 
 import * as React from 'react'
-
-import { cn } from '@/lib/utils'
+import {RadioButtonChecked} from '@/assets/icons/radio-button-checked'
+import {cn} from '@/lib/utils'
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
-import { Circle } from 'lucide-react'
+
+export type Option = {
+  label: string
+  value: string
+}
+
+export interface RadioGroupProps {
+  options: Option[]
+  defaultValue?: string
+  onValueChange?: (value: string) => void
+  className?: string
+  disabled?: boolean
+}
 
 const RadioGroup = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
->(({ className, ...props }, ref) => {
-  return <RadioGroupPrimitive.Root className={cn('grid gap-2', className)} {...props} ref={ref} />
+  RadioGroupProps & React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({className, options, defaultValue, onValueChange, disabled, ...props}, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn('grid gap-2', className)}
+      defaultValue={defaultValue}
+      onValueChange={onValueChange}
+      disabled={disabled}
+      ref={ref}
+      {...props}
+    >
+      {options.map(option => (
+        <div className={'flex items-center gap-0.5'} key={option.value}>
+          <RadioGroupItem id={option.value} value={option.value} disabled={disabled}/>
+          <label className={cn('text-regular_14', disabled ? 'text-dark-100' : 'text-light-100')}
+                 htmlFor={option.value}>
+            {option.label}
+          </label>
+        </div>
+      ))}
+    </RadioGroupPrimitive.Root>
+  )
 })
 
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
@@ -18,29 +49,27 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, ...props }, ref) => {
-  const isDisabled = props.disabled
-
+>(({className, disabled, ...props}, ref) => {
   return (
     <div
-      className={`flex items-center justify-center h-[36px] w-[36px] rounded-full ${isDisabled ? '' : 'hover:bg-dark-300 active:bg-dark-100 focus-visible:bg-dark-500'}
-      `}
+      className={cn(
+        'flex items-center justify-center h-[36px] w-[36px] rounded-full transition-colors',
+        !disabled && 'hover:bg-dark-300 active:bg-dark-100 focus-visible:outline-none focus-visible:bg-dark-500'
+      )}
     >
       <div className={'flex items-center justify-center h-[24px] w-[24px]'}>
         <RadioGroupPrimitive.Item
           className={cn(
-            'h-[20px] w-[20px] rounded-full border-2 border-light-100 bg-transparent disabled:border-dark-100',
+            'flex items-center justify-center h-[20px] w-[20px] rounded-full border-2 border-light-100 bg-transparent',
+            disabled && 'border-dark-100',
             className
           )}
           ref={ref}
+          disabled={disabled}
           {...props}
         >
-          <RadioGroupPrimitive.Indicator
-            className={'flex items-center justify-center h-full w-full rounded-full'}
-          >
-            <Circle
-              className={`h-[10px] w-[10px] fill-current ${isDisabled ? 'text-dark-100' : 'text-light-100'}`}
-            />
+          <RadioGroupPrimitive.Indicator className={'h-[24px] w-[24px]'}>
+            <RadioButtonChecked className={cn(disabled ? 'fill-dark-100' : 'fill-light-100')}/>
           </RadioGroupPrimitive.Indicator>
         </RadioGroupPrimitive.Item>
       </div>
@@ -50,4 +79,4 @@ const RadioGroupItem = React.forwardRef<
 
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
 
-export { RadioGroup, RadioGroupItem }
+export {RadioGroup, RadioGroupItem}
