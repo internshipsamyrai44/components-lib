b9c3a797018f7b75b570e54cb09a79f071986342
JuliaNezhel <yulyazelikova@gmail.com>
2024-09-29 19:50:11 +0300

    feat: add component slider

---
+++ src/components/slider/slider.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { useState } from 'react'
+
+import { Slider } from './slider'
+
+const meta = {
+  component: Slider,
+  tags: ['autodocs'],
+  title: 'Components/Slider',
+} satisfies Meta<typeof Slider>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SliderDefault: Story = {
+  argTypes: {
+    minStepsBetweenThumbs: {
+      control: {
+        type: 'number',
+      },
+      description: 'minimum steps between thumbs if thumbs more than 1',
+    },
+  },
+  args: {
+    max: 100,
+    min: 0,
+    value: [25, 75],
+  },
+  render: () => {
+    const [value, setValue] = useState([25, 75])
+
+    return (
+      <Slider
+        max={100}
+        min={0}
+        minStepsBetweenThumbs={1}
+        onValueChange={(newValue: number[]) => {
+          setValue(newValue)
+        }}
+        step={10}
+        value={value}
+      />
+    )
+  },
+}
