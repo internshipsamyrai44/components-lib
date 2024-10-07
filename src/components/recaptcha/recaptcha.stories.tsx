402cff9e0b6bc7c70db0b0d5e01dc775ed6ff78a
Artem Rybin <catobazzz@gmail.com>
2024-10-07 15:47:14 +0300

    feat: add stories in recaptcha.stories.tsx

--- src/components/recaptcha/recaptcha.stories.tsx
+++ src/components/recaptcha/recaptcha.stories.tsx
@@ -15,3 +15,18 @@ type Story = StoryObj<typeof meta>
 export const Initial: Story = {
   args: { variant: 'default', onClick: action('Button clicked') },
 }
+export const Checked: Story = {
+  args: { variant: 'checked', onClick: action('Button clicked') },
+}
+
+export const Loading: Story = {
+  args: { variant: 'loading', onClick: action('Button clicked') },
+}
+
+export const Expired: Story = {
+  args: { variant: 'expired', onClick: action('Button clicked') },
+}
+
+export const WithError: Story = {
+  args: { variant: 'withError', onClick: action('Button clicked') },
+}
