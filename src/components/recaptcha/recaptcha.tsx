a38534631af285080c9b2671d01dcf96adad780e
Artem Rybin <catobazzz@gmail.com>
2024-10-09 11:15:57 +0300

    style: add css for recaptcha.tsx

--- src/components/recaptcha/recaptcha.tsx
+++ src/components/recaptcha/recaptcha.tsx
@@ -1,7 +1,7 @@
 import { ComponentPropsWithoutRef } from 'react'
 
-import CheckmarkOutline from '@/assets/icons/CheckmarkOutline'
-import Recaptchalogo1 from '@/assets/icons/Recaptchalogo1'
+import CheckmarkOutline from '@/assets/icons/checkmarkOutline'
+import Recaptchalogo1 from '@/assets/icons/recaptchalogo1'
 import { cn } from '@/lib/utils'
 
 type policyLinks = [string, string]
@@ -46,8 +46,8 @@ export const Recaptcha = (props: RecaptchaProps) => {
       >
         <RecaptchaForm onClick={onClick} variant={variant} />
         <div className={'text-light-100 pl-8'}>{label}</div>
-        <div className={'footer'}>
-          <Recaptchalogo1 className={'logo'} />
+        <div className={'w-[44px] text-[5px] text-center'}>
+          <Recaptchalogo1 className={'w-[44px] h-[44px]'} />
           <a className={'hover:underline'} href={Privacy}>
             Privacy
           </a>
@@ -56,9 +56,19 @@ export const Recaptcha = (props: RecaptchaProps) => {
             Terms
           </a>
         </div>
-        {variant === 'expired' && <div className={'expired'}>{expiredMessage}</div>}
+        {variant === 'expired' && (
+          <div className={'text-[10px] text-red-500'}>{expiredMessage}</div>
+        )}
       </div>
-      {variant === 'withError' && <div className={'error'}>{errorMessage}</div>}
+      {variant === 'withError' && (
+        <div
+          className={
+            'absolute top-[2px] left-[20px] w-[185px] text-[10px] font-normal leading-tight text-red-500'
+          }
+        >
+          {errorMessage}
+        </div>
+      )}
     </div>
   )
 }
@@ -73,16 +83,33 @@ const RecaptchaForm = ({ onClick, variant }: RecaptchaFormProps) => {
     case 'default':
     case 'withError':
     case 'expired':
-      // eslint-disable-next-line react/button-has-type
-      return <button className={'checkBox'} onClick={onClick}></button>
+      return (
+        // eslint-disable-next-line react/button-has-type
+        <button
+          className={cn(
+            'btnCheck cursor-pointer absolute top-1/2 left-[20px] -translate-y-1/2 w-[20px] h-[20px] bg-[var(--color-light-100)] border border-[#b7b7b7] transition-colors duration-200 hover:bg-[var(--color-light-700)] active:bg-[var(--color-light-900)]'
+          )}
+          onClick={onClick}
+        ></button>
+      )
     case 'checked':
       return (
-        <div className={'checked'}>
+        <div
+          className={cn(
+            'absolute top-1/2 left-[15px] -translate-y-1/2 inline-block m-0 text-[31px] font-bold text-[#19983b]'
+          )}
+        >
           <CheckmarkOutline />
         </div>
       )
     case 'loading':
-      return <div className={'loader'}></div>
+      return (
+        <div
+          className={
+            'absolute top-1/2 left-[18px] inline-block box-border w-[25px] h-[25px] mr-[6px] border-t-[3px] border-t-[#4d8df4] border-r-[3px] border-r-transparent border-l-[3px] border-l-transparent rounded-full animate-spin'
+          }
+        ></div>
+      )
     default:
       return null
   }
