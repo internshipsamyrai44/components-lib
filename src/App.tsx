1f7ad9c53181230d65d7505521531fc4e8c10ba7
Artem Rybin <catobazzz@gmail.com>
2024-10-18 13:32:16 +0300

    feat: import index in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components'
 
+import './index.css'
+
 export function App() {
   return (
-    <div className={'flex-auto flex-col align-middle wid'}>
+    <div className={'flex-auto flex-col align-middle wid bg-dark-500'}>
       Hello
       <Button variant={'primary'}>Click me</Button>
     </div>
