99a0729a840882f3855bb4cc035ede8210122b17
Artem Rybin <catobazzz@gmail.com>
2024-10-18 14:04:18 +0300

    feat: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components'
 import { AlertPopup } from '@/components/alertPopup/AlertPopup'
+import { Tabs } from '@/components/tabs/tabsRoot'
 
 import './index.css'
 
@@ -9,6 +10,15 @@ export function App() {
       Hello
       <Button variant={'primary'}>Click me</Button>
       <AlertPopup alertType={'error'} message={'Name was successfully changed'} />
+      <Tabs
+        tabs={[
+          { title: 'Tab 1', value: 'tab1' },
+          { disabled: true, title: 'Tab 2', value: 'tab2' },
+          { title: 'Tab 3', value: 'tab3' },
+          { title: 'Tab 4', value: 'tab4' },
+          { title: 'Tab 5', value: 'tab5' },
+        ]}
+      />
     </div>
   )
 }
