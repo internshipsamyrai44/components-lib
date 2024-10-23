36c90c0b0adca520678d611333b1f35ac8d7cba1
Julia <veluat@gmail.com>
2024-10-23 11:58:16 +0300

    INC-12 feat: add active state icons for sidebar items

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -4,6 +4,15 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
+interface SidebarItemProps {
+  className?: string
+  icon: React.ElementType
+  iconActive?: React.ElementType
+  isActive?: boolean
+  isDisabled?: boolean
+  title: string
+}
+
 const Sidebar = React.forwardRef<HTMLDivElement, React.ComponentProps<'div'>>(
   ({ children, className, ...props }, ref) => {
     return (
@@ -67,17 +76,52 @@ const SidebarGroupContent = React.forwardRef<HTMLDivElement, React.ComponentProp
 
 SidebarGroupContent.displayName = 'SidebarGroupContent'
 
-const SidebarLink = React.forwardRef<HTMLAnchorElement, React.ComponentProps<'a'>>(
-  ({ className, ...props }, ref) => (
-    <a
-      className={cn('flex items-center gap-3 text-light-100 text-regular_14', className)}
-      data-sidebar={'link'}
-      ref={ref}
-      {...props}
-    />
-  )
-)
+const SidebarLink = React.forwardRef<
+  HTMLAnchorElement,
+  { isActive?: boolean; isDisabled?: boolean } & React.ComponentProps<'a'>
+>(({ className, isActive, isDisabled, ...props }, ref) => (
+  <a
+    className={cn(
+      'flex items-center gap-3 text-light-100 text-regular_14 size-max rounded-sm border-2 border-input border-transparent',
+      'focus:outline-none focus:border-accent-700',
+      className
+    )}
+    data-sidebar={'link'}
+    ref={ref}
+    {...props}
+  />
+))
 
 SidebarLink.displayName = 'SidebarLink'
 
-export { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarLink }
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  className,
+  icon: Icon,
+  iconActive: IconActive,
+  isActive,
+  isDisabled,
+  title,
+}) => {
+  const IconToRender = isActive ? IconActive : Icon
+  const getClassNames = () => {
+    if (isDisabled) {
+      return 'text-dark-100 fill-dark-100 cursor-not-allowed'
+    }
+    if (isActive) {
+      return 'text-accent-500 text-bold_14'
+    }
+
+    return 'text-light-100 hover:text-accent-100 hover:fill-accent-100'
+  }
+
+  return (
+    <div className={cn('flex items-center gap-3', getClassNames(), className)}>
+      {IconToRender ? <IconToRender /> : null}
+      <span>{title}</span>
+    </div>
+  )
+}
+
+SidebarItem.displayName = 'SidebarItem'
+
+export { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarItem, SidebarLink }
