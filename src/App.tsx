9c8760b1e8c2d8fd5d01cafc770209f657680268
fanur56 <103331549+fanur56@users.noreply.github.com>
2024-10-26 19:11:13 +0300

    feat: Created Checkbox component (#8)
    * feat: Created Checkbox component
    
    * fix: label, css
    
    * fix: css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components'
-import { Pagination } from '@/components/pagination/pagination'
+
+import { cn } from '@/lib/utils'
+
 
 export function App() {
   const changePage = () => {
@@ -7,10 +9,11 @@ export function App() {
   }
 
   return (
-    <div className={'bg-danger-100 flex-auto flex-col align-middle wid'}>
-      Hello
-      <Button variant={'primary'}>Click me</Button>
-      <Pagination currentPage={1} onChangePage={changePage} pageSize={1} totalCount={10} />
+    <div className={cn('bg-danger-100')}>
+      <div className={'flex-auto flex-col align-middle wid'}>
+        Hello
+        <Button variant={'primary'}>Click me</Button>
+      </div>
     </div>
   )
 }
