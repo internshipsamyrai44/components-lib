eec1ebe715b67eee8d4b336b52c5b510fe7262c5
Artem Rybin <catobazzz@gmail.com>
2025-01-14 11:30:50 +0300

    style: fix CSS data-attributes for tabs.module.scss ref: ref Tabs with automatically prettier and Eslint

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,78 +1,76 @@
-import * as React from 'react';
-import { ComponentPropsWithoutRef, ReactNode } from 'react';
-import { cn } from '@/lib/utils';
-import * as TabsPrimitive from '@radix-ui/react-tabs';
-import styles from './tabs.module.scss'; // Импортируем стили
+import * as React from 'react'
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
+
+import { cn } from '@/lib/utils'
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+
+import styles from './tabs.module.scss' // Импортируем стили
 
 export type TabType = {
-    disabled?: boolean;
-    title: string;
-    value: string;
-};
+  disabled?: boolean
+  title: string
+  value: string
+}
 
 type CommonProps = {
-    children?: ReactNode;
-    fullWidth?: boolean;
-    tabs: TabType[];
-} & ComponentPropsWithoutRef<typeof TabsPrimitive.Root>;
+  children?: ReactNode
+  fullWidth?: boolean
+  tabs: TabType[]
+} & ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
 
 const Tabs = React.forwardRef<React.ElementRef<typeof TabsPrimitive.Root>, CommonProps>(
-    ({ children, className, fullWidth, tabs, ...rest }, ref) => (
-        <TabsPrimitive.Root {...rest} ref={ref} className={cn(styles.tabs, fullWidth && styles.fullWidth)}>
-            <TabsList className={cn(fullWidth && styles.fullWidth)}>
-                {tabs.map(tab => (
-                    <TabsTrigger
-                        className={cn(fullWidth && 'grow', styles.tabsTrigger)}
-                        disabled={tab.disabled}
-                        key={tab.value}
-                        value={tab.value}
-                    >
-                        {tab.title}
-                    </TabsTrigger>
-                ))}
-            </TabsList>
-            {children}
-        </TabsPrimitive.Root>
-    )
-);
+  ({ children, className, fullWidth, tabs, ...rest }, ref) => (
+    <TabsPrimitive.Root
+      {...rest}
+      className={cn(styles.tabs, fullWidth && styles.fullWidth)}
+      ref={ref}
+    >
+      <TabsList className={cn(fullWidth && styles.fullWidth)}>
+        {tabs.map(tab => (
+          <TabsTrigger
+            className={cn(fullWidth && 'grow', styles.tabsTrigger)}
+            disabled={tab.disabled}
+            key={tab.value}
+            value={tab.value}
+          >
+            {tab.title}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {children}
+    </TabsPrimitive.Root>
+  )
+)
 
-Tabs.displayName = 'Tabs';
+Tabs.displayName = 'Tabs'
 
 const TabsList = React.forwardRef<
-    React.ElementRef<typeof TabsPrimitive.List>,
-    React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
 >(({ className, ...props }, ref) => (
-    <TabsPrimitive.List
-        className={cn(styles.tabsList, className)}
-        ref={ref}
-        {...props}
-    />
-));
+  <TabsPrimitive.List className={cn(styles.tabsList, className)} ref={ref} {...props} />
+))
 
-TabsList.displayName = 'TabsList';
+TabsList.displayName = 'TabsList'
 
-export type TabsTriggerProps = React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>;
+export type TabsTriggerProps = React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 
 const TabsTrigger = React.forwardRef<
-    React.ElementRef<typeof TabsPrimitive.Trigger>,
-    TabsTriggerProps
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  TabsTriggerProps
 >(({ className, ...props }, ref) => (
-    <TabsPrimitive.Trigger
-        className={cn(styles.tabsTrigger, className)}
-        ref={ref}
-        {...props}
-    />
-));
+  <TabsPrimitive.Trigger className={cn(styles.tabsTrigger, className)} ref={ref} {...props} />
+))
 
-TabsTrigger.displayName = 'TabsTrigger';
+TabsTrigger.displayName = 'TabsTrigger'
 
 const TabsContent = React.forwardRef<
-    React.ElementRef<typeof TabsPrimitive.Content>,
-    React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
 >(({ className, ...props }, ref) => (
-    <TabsPrimitive.Content className={cn(styles.tabsContent, className)} ref={ref} {...props} />
-));
+  <TabsPrimitive.Content className={cn(styles.tabsContent, className)} ref={ref} {...props} />
+))
 
-TabsContent.displayName = 'TabsContent';
+TabsContent.displayName = 'TabsContent'
 
-export { Tabs, TabsContent, TabsList, TabsTrigger };
+export { Tabs, TabsContent, TabsList, TabsTrigger }
